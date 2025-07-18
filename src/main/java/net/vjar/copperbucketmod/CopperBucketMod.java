216f27d27428679f7a9604380e21680c475d49ec
Arman Jabari <armanjabari786@gmail.com>
2025-07-18 22:25:30 +0330

    Initial commit

---
+++ src/main/java/net/vjar/copperbucketmod/CopperBucketMod.java
@@ -0,0 +1,15 @@
+package net.vjar.copperbucketmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CopperBucketMod implements ModInitializer {
+	public static final String MOD_ID = "copper-bucket-mod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/net/vjar/copperbucketmod/CopperBucketModClient.java
@@ -0,0 +1,10 @@
+package net.vjar.copperbucketmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CopperBucketModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
