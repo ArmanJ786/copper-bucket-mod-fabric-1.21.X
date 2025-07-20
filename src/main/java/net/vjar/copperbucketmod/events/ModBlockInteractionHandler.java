6446945a24770a10c4ad834a738adceada32461b
Arman Jabari <armanjabari786@gmail.com>
2025-07-20 20:24:34 +0330

    ...

--- src/main/java/net/vjar/copperbucketmod/events/ModBlockInteractionHandler.java
+++ src/main/java/net/vjar/copperbucketmod/events/ModBlockInteractionHandler.java
@@ -18,10 +18,12 @@ public class ModBlockInteractionHandler {
 
                 if (heldItem.isOf(ModItems.CREAM_BUCKET)) {
                     ItemStack newItem = new ItemStack(ModItems.ICE_CREAM_BUCKET);
-
                     player.setStackInHand(hand, newItem);
 
                     world.playSound(null, pos, SoundEvents.BLOCK_SNOW_BREAK, SoundCategory.PLAYERS, 1.0F, 1.0F);
+                    world.playSound(null, pos, SoundEvents.BLOCK_AMETHYST_BLOCK_HIT, SoundCategory.PLAYERS, 0.8F, 1.3F);
+
+                    world.breakBlock(pos, false);
 
                     return ActionResult.SUCCESS;
                 }
